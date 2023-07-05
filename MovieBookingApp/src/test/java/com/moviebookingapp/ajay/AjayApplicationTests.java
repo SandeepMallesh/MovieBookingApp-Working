edc4d0d69590ec2c35326f663504b468b80c61c8
meena <aj@gmail.com>
2023-07-05 20:07:21 +0530

    Added Swagger

--- MovieBookingApp/src/test/java/com/moviebookingapp/ajay/AjayApplicationTests.java
+++ MovieBookingApp/src/test/java/com/moviebookingapp/ajay/AjayApplicationTests.java
@@ -8,8 +8,10 @@ package com.moviebookingapp.ajay;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootTest
+@EnableSwagger2
 public class AjayApplicationTests {
 
 	@Test
